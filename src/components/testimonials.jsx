bbb5c04cfe4b687a0bfccb2bd4d0b7bebc37e5d1
Carlos Del Real <c.humberto@guideline-contractors.com>
2025-04-20 07:27:01 +0200

    feat: fix responsiveness

--- src/components/testimonials.jsx
+++ src/components/testimonials.jsx
@@ -7,7 +7,7 @@ export const Testimonials = (props) => {
         <div className="section-title text-center">
           <h2>What our clients say</h2>
         </div>
-        <div className="row">
+        <div className="row" style={{ margin: 0 }}>
           {props.data
             ? props.data.map((d, i) => (
                 <div key={`${d.name}-${i}`} className="col-md-4">
