958b8cd3714ae6eb000bf2a0ff8ec285cca76b26
Carlos Del Real <carloshdelreal@gmail.com>
2025-09-07 17:49:04 -0500

    feat: fixed grid

--- src/components/gallery.jsx
+++ src/components/gallery.jsx
@@ -9,23 +9,21 @@ export const Gallery = ({ data, description }) => {
           <h2>Gallery</h2>
           <p>{description}</p>
         </div>
-        <div className="row">
-          <div className="portfolio-items">
-            {data
-              ? data.map((d, i) => (
-                  <div
-                    key={`${d.title}-${i}`}
-                    className="col-sm-6 col-md-4 col-lg-4"
-                  >
-                    <Image
-                      title={d.title}
-                      largeImage={d.largeImage}
-                      smallImage={d.smallImage}
-                    />
-                  </div>
-                ))
-              : "Loading..."}
-          </div>
+        <div className="portfolio-masonry">
+          {data
+            ? data.map((d, i) => (
+                <div
+                  key={`${d.title}-${i}`}
+                  className="portfolio-item-wrapper"
+                >
+                  <Image
+                    title={d.title}
+                    largeImage={d.largeImage}
+                    smallImage={d.smallImage}
+                  />
+                </div>
+              ))
+            : "Loading..."}
         </div>
       </div>
     </div>
